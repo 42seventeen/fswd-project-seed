8a878bba095eecb7d4c8feb6856cfcb38b6e0510
Jonathan Herndon <jonathan@42seventeen.net>
2016-11-16 19:33:40 -0500

    Add new fields to model
    + Name
    + Role

--- models/user.js
+++ models/user.js
@@ -4,9 +4,15 @@ var bcrypt = require('bcrypt');
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+    },
     username: {
       type: DataTypes.STRING,
       unique: true
+    },
+    role: {
+      type: DataTypes.STRING,
     },
     password: {
       type: DataTypes.STRING,
