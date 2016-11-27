fd92b006954afce4642ba4ac9c774029bab8bb06
Jonathan Herndon <jonathan@42seventeen.net>
2016-11-27 16:48:49 -0500

    initial commit of webcast model

---
+++ models/webcast.js
@@ -0,0 +1,40 @@
+// Webcast Model Definition
+
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Webcast = sequelize.define('Webcast', {
+    title: {
+      type: DataTypes.STRING
+    },
+    webcastDate: {
+      type: DataTypes.DATEONLY
+    },
+    venue: {
+      type: DataTypes.STRING
+    },
+    clientName: {
+      type: DataTypes.STRING
+    },
+    clientEmail: {
+      type: DataTypes.STRING
+    },
+    UserId: {
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+    instanceMethods: {
+    },
+    classMethods: {
+      associate: function(models) {
+        Webcast.belongsTo(models.User),
+        Webcast.hasOne(models.Channel)
+      }
+    },
+    validate: {
+      // Validation stuff to come later
+    }
+  });
+  return Webcast;
+};
