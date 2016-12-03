5742eadebd342b30750e060a78cafedeacaebf2b
Jonathan Herndon <jonathan@42seventeen.net>
2016-12-02 19:51:28 -0500

    adds a views and methods to return the webcasts associated with a specific developers
    Used on the a user detail page

--- models/webcast.js
+++ models/webcast.js
@@ -22,6 +22,9 @@ module.exports = function(sequelize, DataTypes) {
     UserId: {
       type: DataTypes.INTEGER
     },
+    devId: {
+      type: DataTypes.INTEGER
+    },
     ChannelId: {
       type: DataTypes.INTEGER
     }
